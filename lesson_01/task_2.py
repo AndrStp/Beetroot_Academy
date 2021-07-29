93a386a75cabd3135e128962ff8af65614651ec5
Andriy Stepanenko <ast.university@hotmail.com>
2021-07-29 10:00:59 +0300

    Initial commit. Added homework for lesson 01

---
+++ lesson_01/task_2.py
@@ -0,0 +1,5 @@
+# prints 5, 4, 3, 2, 1 separate on each line
+print(5, 4, 3, 2, 1, sep='\n')
+
+# prints 'Andrew' with 5 exclamation marks and new-line char at the end
+print('Andrew', end='!!!!!\n')
---
+++ lesson_01/task_3.py
@@ -0,0 +1,11 @@
+# letter 'O'
+roof = '#########\n'
+wall = '#\t#\n'
+
+print(roof + wall * 3 + roof)
+
+# letter 'H'
+top_and_bottom = '#\t#\n'
+middle = '#########\n'
+
+print(top_and_bottom * 2 + middle + top_and_bottom * 2)
