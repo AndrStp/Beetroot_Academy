7237ca30b2664eb3726c73e50734b9c1d4ea39d4
Andriy Stepanenko <ast.university@hotmail.com>
2021-09-06 17:24:12 +0300

    adedd task_1.py for lesson_20

---
+++ lesson_20/task_1.py
@@ -0,0 +1,48 @@
+"""
+File Context Manager class
+
+Create your own class, which can behave like a built-in function `open`. 
+Also, you need to extend its functionality with counter and logging. 
+Pay special attention to the implementation of `__exit__` method, which has to 
+cover all the requirements to context managers mentioned here:
+
+https://docs.python.org/3.7/library/stdtypes.html#typecontextmanager 
+https://docs.python.org/3.7/reference/compound_stmts.html#with
+"""
+
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d %b %Y %H:%M:%S', level=logging.DEBUG)
+
+
+class FileManager:
+    counter = 0
+
+    @classmethod
+    def get_counter(cls):
+        return cls.counter
+
+    def __init__(self, file_path, mode):
+        self.file_path = file_path
+        self.mode = mode
+        logging.debug('Getting the parametres')
+        
+    def __enter__(self):
+        FileManager.counter += 1
+        self.file_handle = open(self.file_path, self.mode)
+        logging.debug('Entering the context manager')
+        return self.file_handle
+    
+    def __exit__(self, *exc):
+        self.file_handle.close()
+        logging.debug('Leaving the context manager')
+
+
+if __name__ == '__main__':
+    for _ in range(3):
+        with FileManager('text.txt', 'r') as f:
+            print(f.read())
+        print()
+
+    print(FileManager.get_counter())  # -> 3
