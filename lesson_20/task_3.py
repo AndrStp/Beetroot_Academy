dc48746a7b90c6ca13f06588c780b975cc8d9e2c
Andriy Stepanenko <ast.university@hotmail.com>
2021-09-10 12:12:54 +0300

    problems with task_3

--- lesson_20/task_3.py
+++ lesson_20/task_3.py
@@ -9,3 +9,19 @@ Create a test case for this function using pytest library (https://docs.pytest.o
 Create pytest fixture, which uses your implementation of the context manager to return a file object, 
 which could be used inside your function.
 """
+
+import pytest
+import sys
+from task_1 import FileManager
+
+
+# @pytest.fixture
+# def return_file_object(file_name: str):
+#     with FileManager(file_name, 'r') as f:
+#         return 
+
+
+def test(file_obj) -> int:
+    """Returns the size of an file object"""
+    with file_obj as f:
+        return sys.getsizeof(file_obj)
